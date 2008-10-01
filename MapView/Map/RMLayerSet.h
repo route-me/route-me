569ec416ce1a3145da9addadaa3c061636a2e258
Joseph G <josephg@gmail.com>
2008-10-01 02:40:03 +0000

    Urgh fixed broken build

---
+++ MapView/Map/RMLayerSet.h
@@ -0,0 +1,18 @@
+//
+//  RMLayerSet.h
+//  MapView
+//
+//  Created by Joseph Gentle on 1/10/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol RMMapLayer;
+
+@interface RMLayerSet : NSObject
+{
+	
+}
+
+@end
---
+++ MapView/Map/RMLayerSet.m
@@ -0,0 +1,14 @@
+//
+//  RMLayerSet.m
+//  MapView
+//
+//  Created by Joseph Gentle on 1/10/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "RMLayerSet.h"
+
+
+@implementation RMLayerSet
+
+@end
