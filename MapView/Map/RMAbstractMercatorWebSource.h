f44feef31a66228a344a13aaf2ea08f08b699d04
Vladimir Vyskocil <vladimir.vyskocil@gmail.com>
2010-04-15 20:05:25 +0000

    declare setTileSideLength:

--- MapView/Map/RMAbstractMercatorWebSource.h
+++ MapView/Map/RMAbstractMercatorWebSource.h
@@ -51,6 +51,7 @@
 -(void) networkOperationsNotification: (NSNotification*) notification;
 
 -(int) tileSideLength;
+-(void) setTileSideLength: (NSUInteger) aTileSideLength;
 
 -(float) minZoom;
 -(float) maxZoom;
