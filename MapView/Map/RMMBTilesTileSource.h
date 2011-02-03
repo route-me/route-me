180c4e32ceb64386b91eb868d247aa5f5d52082c
Justin R. Miller <incanus@codesorcery.net>
2011-02-03 12:04:23 -0800

    added MBTiles format tile source

---
+++ MapView/Map/RMMBTilesTileSource.h
@@ -0,0 +1,77 @@
+//
+//  RMMBTilesTileSource.h
+//
+//  Created by Justin R. Miller on 6/18/10.
+//  Copyright 2010, Code Sorcery Workshop, LLC and Development Seed, Inc.
+//  All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//  
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//  
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//  
+//      * Neither the names of Code Sorcery Workshop, LLC or Development Seed,
+//        Inc., nor the names of its contributors may be used to endorse or
+//        promote products derived from this software without specific prior
+//        written permission.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+//  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  http://mapbox.com/documentation/mbtiles-file-format
+//
+
+#import <Foundation/Foundation.h>
+#import "RMTileSource.h"
+
+@class RMFractalTileProjection;
+@class FMDatabase;
+
+#define kMBTilesDefaultTileSize 256
+#define kMBTilesDefaultMinTileZoom 0
+#define kMBTilesDefaultMaxTileZoom 18
+#define kMBTilesDefaultLatLonBoundingBox ((RMSphericalTrapezium){ .northeast = { .latitude =  90, .longitude =  180 }, \
+                                                                  .southwest = { .latitude = -90, .longitude = -180 } })
+
+@interface RMMBTilesTileSource : NSObject <RMTileSource>
+{
+    RMFractalTileProjection *tileProjection;
+    FMDatabase *db;
+}
+
+- (id)initWithTileSetURL:(NSURL *)tileSetURL;
+- (int)tileSideLength;
+- (void)setTileSideLength:(NSUInteger)aTileSideLength;
+- (RMTileImage *)tileImage:(RMTile)tile;
+- (NSString *)tileURL:(RMTile)tile;
+- (NSString *)tileFile:(RMTile)tile;
+- (NSString *)tilePath;
+- (id <RMMercatorToTileProjection>)mercatorToTileProjection;
+- (RMProjection *)projection;
+- (float)minZoom;
+- (float)maxZoom;
+- (void)setMinZoom:(NSUInteger)aMinZoom;
+- (void)setMaxZoom:(NSUInteger)aMaxZoom;
+- (RMSphericalTrapezium)latitudeLongitudeBoundingBox;
+- (void)didReceiveMemoryWarning;
+- (NSString *)uniqueTilecacheKey;
+- (NSString *)shortName;
+- (NSString *)longDescription;
+- (NSString *)shortAttribution;
+- (NSString *)longAttribution;
+- (void)removeAllCachedImages;
+
+@end
---
+++ MapView/Map/RMMBTilesTileSource.m
@@ -0,0 +1,258 @@
+//
+//  RMMBTilesTileSource.m
+//
+//  Created by Justin R. Miller on 6/18/10.
+//  Copyright 2010, Code Sorcery Workshop, LLC and Development Seed, Inc.
+//  All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//  
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//  
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//  
+//      * Neither the names of Code Sorcery Workshop, LLC or Development Seed,
+//        Inc., nor the names of its contributors may be used to endorse or
+//        promote products derived from this software without specific prior
+//        written permission.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+//  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "RMMBTilesTileSource.h"
+#import "RMTileImage.h"
+#import "RMProjection.h"
+#import "RMFractalTileProjection.h"
+#import "FMDatabase.h"
+
+#pragma mark -
+
+@implementation RMMBTilesTileSource
+
+- (id)initWithTileSetURL:(NSURL *)tileSetURL
+{
+	if ( ! [super init])
+		return nil;
+	
+	tileProjection = [[RMFractalTileProjection alloc] initFromProjection:[self projection] 
+                                                          tileSideLength:kMBTilesDefaultTileSize 
+                                                                 maxZoom:kMBTilesDefaultMaxTileZoom 
+                                                                 minZoom:kMBTilesDefaultMinTileZoom];
+	
+    db = [[FMDatabase databaseWithPath:[tileSetURL relativePath]] retain];
+    
+    if ( ! [db open])
+        return nil;
+    
+	return self;
+}
+
+- (void)dealloc
+{
+	[tileProjection release];
+    
+    [db close];
+    [db release];
+    
+	[super dealloc];
+}
+
+- (int)tileSideLength
+{
+	return tileProjection.tileSideLength;
+}
+
+- (void)setTileSideLength:(NSUInteger)aTileSideLength
+{
+	[tileProjection setTileSideLength:aTileSideLength];
+}
+
+- (RMTileImage *)tileImage:(RMTile)tile
+{
+    NSAssert4(((tile.zoom >= self.minZoom) && (tile.zoom <= self.maxZoom)),
+			  @"%@ tried to retrieve tile with zoomLevel %d, outside source's defined range %f to %f", 
+			  self, tile.zoom, self.minZoom, self.maxZoom);
+
+    NSInteger zoom = tile.zoom;
+    NSInteger x    = tile.x;
+    NSInteger y    = pow(2, zoom) - tile.y - 1;
+
+    FMResultSet *results = [db executeQuery:@"select tile_data from tiles where zoom_level = ? and tile_column = ? and tile_row = ?", 
+                               [NSNumber numberWithFloat:zoom], 
+                               [NSNumber numberWithFloat:x], 
+                               [NSNumber numberWithFloat:y]];
+    
+    if ([db hadError])
+        return [RMTileImage dummyTile:tile];
+    
+    [results next];
+    
+    NSData *data = [results dataForColumn:@"tile_data"];
+
+    RMTileImage *image;
+    
+    if ( ! data)
+        image = [RMTileImage dummyTile:tile];
+    
+    else
+        image = [RMTileImage imageForTile:tile withData:data];
+    
+    [results close];
+    
+    return image;
+}
+
+- (NSString *)tileURL:(RMTile)tile
+{
+    return nil;
+}
+
+- (NSString *)tileFile:(RMTile)tile
+{
+    return nil;
+}
+
+- (NSString *)tilePath
+{
+    return nil;
+}
+
+- (id <RMMercatorToTileProjection>)mercatorToTileProjection
+{
+	return [[tileProjection retain] autorelease];
+}
+
+- (RMProjection *)projection
+{
+	return [RMProjection googleProjection];
+}
+
+- (float)minZoom
+{
+    FMResultSet *results = [db executeQuery:@"select min(zoom_level) from tiles"];
+    
+    if ([db hadError])
+        return kMBTilesDefaultMinTileZoom;
+    
+    [results next];
+    
+    double minZoom = [results doubleForColumnIndex:0];
+    
+    [results close];
+    
+    return (float)minZoom;
+}
+
+- (float)maxZoom
+{
+    FMResultSet *results = [db executeQuery:@"select max(zoom_level) from tiles"];
+    
+    if ([db hadError])
+        return kMBTilesDefaultMaxTileZoom;
+
+    [results next];
+    
+    double maxZoom = [results doubleForColumnIndex:0];
+    
+    [results close];
+    
+    return (float)maxZoom;
+}
+
+- (void)setMinZoom:(NSUInteger)aMinZoom
+{
+    [tileProjection setMinZoom:aMinZoom];
+}
+
+- (void)setMaxZoom:(NSUInteger)aMaxZoom
+{
+    [tileProjection setMaxZoom:aMaxZoom];
+}
+
+- (RMSphericalTrapezium)latitudeLongitudeBoundingBox
+{
+    return kMBTilesDefaultLatLonBoundingBox;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    NSLog(@"*** didReceiveMemoryWarning in %@", [self class]);
+}
+
+- (NSString *)uniqueTilecacheKey
+{
+    return [NSString stringWithFormat:@"MBTiles%@", [[db databasePath] lastPathComponent]];
+}
+
+- (NSString *)shortName
+{
+    FMResultSet *results = [db executeQuery:@"select value from metadata where name = 'name'"];
+    
+    if ([db hadError])
+        return @"Unknown MBTiles";
+    
+    [results next];
+    
+    NSString *shortName = [results stringForColumnIndex:0];
+    
+    [results close];
+    
+    return shortName;
+}
+
+- (NSString *)longDescription
+{
+    FMResultSet *results = [db executeQuery:@"select value from metadata where name = 'description'"];
+    
+    if ([db hadError])
+        return @"Unknown MBTiles description";
+    
+    [results next];
+    
+    NSString *description = [results stringForColumnIndex:0];
+    
+    [results close];
+    
+    return [NSString stringWithFormat:@"%@ - %@", [self shortName], description];
+}
+
+- (NSString *)shortAttribution
+{
+    FMResultSet *results = [db executeQuery:@"select value from metadata where name = 'attribution'"];
+    
+    if ([db hadError])
+        return @"Unknown MBTiles attribution";
+    
+    [results next];
+    
+    NSString *attribution = [results stringForColumnIndex:0];
+    
+    [results close];
+    
+    return attribution;
+}
+
+- (NSString *)longAttribution
+{
+    return [NSString stringWithFormat:@"%@ - %@", [self shortName], [self shortAttribution]];
+}
+
+- (void)removeAllCachedImages
+{
+    NSLog(@"*** removeAllCachedImages in %@", [self class]);
+}
+
+@end
